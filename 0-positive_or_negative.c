42dde2cf51ce36a2752b6c31e5e2ba93c10408f2
celestuwa <celestuwa@gmail.com>
2022-04-08 17:23:00 -0700

    Task 0

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -16,12 +16,12 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n > 1)
-printf("%i is positive\n", n);
+printf("% is positive\n", n);
 else if (n < 0)
-printf("%i is negative\n", n);
+printf("% is negative\n", n);
 else
 
-printf("%i is zero\n", n);
+printf("% is zero\n", n);
 
 return (0);
 }
