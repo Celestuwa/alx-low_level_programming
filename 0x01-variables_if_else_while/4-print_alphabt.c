1a897d639bc2a4ac51ef1f088a171affd63ec776
celestuwa <celestuwa@gmail.com>
2022-04-09 04:03:38 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+
 /**
- * main - main block
- *  Description: Get a random number and check its last digit, compare it with 5
- *  Return: 0
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-char c = 'a';
-while (c <= 'z')
-{
-if (c != 'e' && c != 'q')
-{
-putchar(c);
-{
-c++;
-}
-putchar('\n');
-return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
