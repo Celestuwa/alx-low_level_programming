efc1e585866e62e33873dd763d70532e36a13eca
celestuwa <celestuwa@gmail.com>
2022-04-08 17:36:41 -0700

    Task 0

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
   * main - Entry point
   *
@@ -9,20 +10,22 @@
 int main(void)
 {
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (n > 0)
+
+if (n > 1)
 {
 printf("%d is positive\n", n);
 }
-if (n < 0)
+else if (n < 0)
 {
 printf("%d is negative\n", n);
 }
-if (n == 0)
+else if (n == 0)
 {
 printf("%d is zero\n", n);
 }
+
 return (0);
 }
