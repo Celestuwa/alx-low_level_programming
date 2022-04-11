8bb5b4d296ebba42614b66c30a12ce4da481d831
celestuwa <celestuwa@gmail.com>
2022-04-11 04:07:18 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - Prints the numbers from 00 to 99, numbers separated by
- * 	 a comma followed by a space, in ascending order.
- *
- * Return: Always 0.
+ * main - entry point
+ * description: prints all possible different combinations of two digits
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 10
+ * Print only the smallest combinationof two digits
+ * Numbers should be printed in ascending order, with two digits
+ * Numbers must be separated by ,, followed by a space
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int digit1, digit2;
+int tens;
+int ones;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
-	{
-		for (digit2 = 0; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 9 && digit2 == 9)
-				continue;
-
-			putchar(',');
-			putchar(' ');
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
+for (tens = 0; tens <= 9; tens++)
+{
+for (ones = tens + 1; ones <= 9; ones++)
+{
+putchar(tens + '0');
+putchar(ones + '0')
+if (tens < 8)
+{
+putchart(',')
+putchart(' ')
+}
+}
+}
+putchar('\n');
+return (0);
 }
