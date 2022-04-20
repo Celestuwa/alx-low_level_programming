4e3dd33b459da4dfe735a45a5e6f9a69b5766451
celestuwa <celestuwa@gmail.com>
2022-04-20 13:49:44 -0700

    Task 0

--- 0-reset_to_98.c
+++ 0-reset_to_98.c
@@ -1,10 +1,11 @@
-#includ "main.h"
+#include"main.h"
 
 /**
  * reset_to_98 - function that takes a pointer to an integer parameter
  * @n: integer pointer
  * Return: 0 Success
  */
+
 void reset_to_98(int *n)
 {
 *n = 98;
