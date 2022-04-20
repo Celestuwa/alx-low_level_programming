7b18b220c187759f916b257a151bc53b8cdb6411
Celestuwa <101522848+Celestuwa@users.noreply.github.com>
2022-04-21 01:08:35 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,30 +1,103 @@
-include "main.h"
+#include "main.h"
+
 /**
- * _atoi - int
- * @s: pointer
- * Return: int.
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
-	
-int _atoi(char *s)
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * idx_num_starts - find index where a digit is first found in string
+ * @s: string to search
+ * Return: integer index where digit is first found
+ */
+
+int idx_num_starts(char *s)
 {
 	int i;
-	int res = 0;
-	int sig = -1;
-	int brk = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; i < _strlen(s); i++)
 	{
-		if (s[i] == '-')
-			sig = sig * -1;
 		if (s[i] >= '0' && s[i] <= '9')
-		{
-			res = res * 10;
-			res -= (s[i] - '0');
-			brk = 1;
-		}
-		else if (brk == 1)
-			break;
+			return (i);
 	}
-	res = sig * res;
-	return (res);
+	return (-1); /* return -1 if no digits found */
+}
+
+/**
+ * find_sign - determine if integer is negative
+ * @s: integer
+ * Return: integer 1 or -1
+ */
+int find_sign(char *s)
+{
+	int negatives = 0, i = 0, sign = 1;
+
+	while (i < (idx_num_starts(s)))
+	{
+		if (s[i++] == '-')
+			negatives++;
+	}
+
+	if (negatives % 2 != 0)
+		sign = -1;
+
+	return (sign);
+}
+
+/**
+ * _atoi - convert string to int
+ * @s: string to convert
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+
+	int idx_digit_starts = (idx_num_starts(s));
+	int sign;
+	int digits_to_print = 0;
+	int t = 1, i;
+	unsigned int num = 0;
+	int digit = (idx_num_starts(s));
+
+	if (idx_digit_starts < 0) /* if no digits found, exit program */
+		return (0);
+
+	sign = find_sign(s);
+
+	while ((s[idx_digit_starts] >= '0' && s[idx_digit_starts] <= '9')
+	       && (idx_digit_starts <= _strlen(s))) /* count digits to print */
+	{
+		digits_to_print += 1;
+		idx_digit_starts++;
+	}
+
+	i = 1;
+	while (i < digits_to_print) /* find powers of ten to multiply places */
+	{
+		t *= 10;
+		i++;
+	}
+
+	for (i = digit; i < (digit + digits_to_print); i++) /* calculate num */
+	{
+		num += (s[i] - '0') * t;
+		t /= 10;
+	}
+
+	return (num * sign);
 }
